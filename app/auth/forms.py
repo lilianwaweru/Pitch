dc4db8fb6454f922089e5864ac8ceb30441290bd
lilian waweru <lilowesh.lw@gmail.com>
2019-04-22 22:25:09 +0300

    added a login route in the views.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -12,7 +12,7 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
- class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     # .......
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
@@ -21,4 +21,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
-  
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')          
