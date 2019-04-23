bbe42b5b432210b1e2e7c7a825f46bb21841c8a1
lilian waweru <lilowesh.lw@gmail.com>
2019-04-23 07:01:28 +0300

    imported the photos instance

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,18 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import User
 from .forms import ReviewForm,UpdateProfile
-from .. import db
+from .. import db,photos
 from flask_login import login_required
 
 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html",user = user)
+    
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
@@ -25,3 +33,15 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+    
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
