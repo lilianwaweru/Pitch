851b9edd84ea54d6f9e3e7618573a20e9964ec1e
lilian waweru <lilowesh.lw@gmail.com>
2019-04-23 03:41:31 +0300

    added an update html file in the profile folder

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,27 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import User
+from .forms import ReviewForm,UpdateProfile
+from .. import db
 from flask_login import login_required
 
 
 
-@main.route('/user/<uname>')
-def profile(uname):
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
-
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user)
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
