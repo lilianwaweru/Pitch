9476e5e9e88555d37cb75671b8df124118db9102
lilian waweru <lilowesh.lw@gmail.com>
2019-04-23 03:16:24 +0300

    imported user from models in the views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,15 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
+from ..models import User
 from flask_login import login_required
 
 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
--- app/models.py
+++ app/models.py
@@ -10,11 +10,16 @@ def load_user(user_id):
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
 
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+
+    
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
